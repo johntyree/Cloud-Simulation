525c36e7f3411311237fc83cf9881ee8f5ebd554
John Tyree <johntyree@gmail.com>
2011-05-03 22:22:00 +0200

    Working on my erlang cloud sim

---
+++ include/constants.erl
@@ -0,0 +1,22 @@
+-module(constants).
+-compile(export_all). %% Probably replace with -export([funcs])
+-author("John Tyree").
+
+Dick = foo.
+
+saturation_pressure(C) when C >= -50, C =< 102 ->
+    T = 273.16 + C,
+    math:pow(10,
+        -7.90298
+        * (373.16/T - 1)
+        + 5.02808
+        * math:log10(373.16/T)
+        - 0.00000013816
+        * (math:pow(10, 11.344
+                * (1 - T/373.16))
+                - 1)
+        + 0.0081328
+        * (math:pow(10, -3.49149
+                * (373.16/T - 1))
+                - 1)
+        + math:log10(1013.246)).
