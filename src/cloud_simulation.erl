6e4346d1fc0e6c3bc98a0c0db7e880a15740a462
John Tyree <johntyree@gmail.com>
2011-05-06 19:18:57 +0200

    Start fleshing out the initialization process.

--- src/cloud_simulation.erl
+++ src/cloud_simulation.erl
@@ -3,12 +3,29 @@
 
 -include("constants.hrl").
 
-main(0) ->
-    io:format("~p~n", [gaussian(0, 1)]),
+main(Args) ->
+    initial_config(),
+    L = deity:init(Args),
+    %io:format("~p~n", [L]),
+    [io:format("~p ~p ~p~n", [X, Y, Z]) || {{X, Y}, {_, Z}} <- L],
+    erlang:halt(0).
+
+
+
+initial_config() ->
+    error_logger:logfile({open, "log"}).
+
+run(0) ->
+    {X, Y} = drop:new_position(),
+    io:format("~p ~p~n", [X, Y]),
+    %io:format("~p~n", [drop:new_size()]),
+    %io:format("~p~n", [gaussian(100, 4.1)]),
     %io:format("~p~n", [random:uniform()]),
     erlang:halt(0);
-main(N) ->
-    io:format("~p ", [gaussian(0, 1)]),
+run(N) ->
+    {X, Y} = drop:new_position(),
+    io:format("~p ~p ", [X, Y]),
+    %io:format("~p ", [gaussian(100, 4.1)]),
     %io:format("~p ", [random:uniform()]),
-    main(N-1).
+    run(N-1).
 
--- src/deity.erl
+++ src/deity.erl
@@ -2,4 +2,28 @@
 -compile(export_all). %% Probably replace with -export([funcs])
 -author("John Tyree").
 
+init(N) when N > 0 ->
+    Drop_List = populate_domain(N),
+    Drop_List.
 
+populate_domain(N) ->
+    Collector = spawn(?MODULE, collect_drops, [N, self()]),
+    populate_domain(N, [], Collector),
+    receive
+        {droplist, L} ->
+            L
+    end.
+populate_domain(0, Acc, _) -> Acc;
+populate_domain(N, Acc, Collector) ->
+    Drop = spawn(drop, init, [Collector, 0]),
+    populate_domain(N-1, [Drop|Acc], Collector).
+
+collect_drops(N, Server) when N >= 0, is_pid(Server) ->
+    collect_drops(N, [], Server).
+collect_drops(0, Acc, Server) ->
+    Server ! {droplist, Acc};
+collect_drops(N, Acc, Server) ->
+    receive
+        {newdrop, DropPid, {Position, Size}} ->
+            collect_drops(N-1, [{Position, {DropPid, Size}} | Acc], Server)
+    end.
