04be6539ee4beb0554d7dc732aa9e7f0a5e340f8
John Tyree <johntyree@gmail.com>
2011-05-15 12:38:44 +0200

    ?GRIDSIZE is the number of points. x1/x2 contain boundary coords.
    Example:
        x1 -> 0
        x2 -> 29
        GRIDSIZE_X -> 30

--- include/node.hrl
+++ include/node.hrl
@@ -2,9 +2,9 @@
         x1 = 0,
         y1 = 0,
         z1 = 0,
-        x2 = ?GRIDSIZE_X,
-        y2 = ?GRIDSIZE_Y,
-        z2 = ?GRIDSIZE_Z,
+        x2 = ?GRIDSIZE_X - 1,
+        y2 = ?GRIDSIZE_Y - 1,
+        z2 = ?GRIDSIZE_Z - 1,
         drops = dict:new(),
         parent}).
 
